8db4125d4536cca98863f044f725a7aab7b1666c
My Tran <mytran@usrosmytran1.myfiosgateway.com>
2021-07-25 13:56:42 -0400

    sadd reducer to thunk without blockers

--- src/todos/reducers.js
+++ src/todos/reducers.js
@@ -2,7 +2,24 @@
  import {
     CREATE_TODO,
     REMOVE_TODO,
-    MARK_TODO_AS_COMPLETED } from './actions';
+    MARK_TODO_AS_COMPLETED,
+    LOAD_TODOS_IN_PROGRESS,
+    LOAD_TODOS_SUCCESS,
+    LOAD_TODOS_FAILURE, } from './actions';
+
+export const isLoading = (state = false, action) => {
+    const { type } = action;
+
+    switch (type) {
+    case LOAD_TODOS_IN_PROGRESS:
+        return true;
+    case LOAD_TODOS_SUCCESS:
+    case LOAD_TODOS_FAILURE:
+        return false;
+    default:
+        return state;
+    }
+}
  
  export const todos = (state = [], action) => {
      const { type, payload } = action;
