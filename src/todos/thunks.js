0a82d00ccaf569b8f3ebb92795cce0a011bfb77a
My Tran <mytran@usrosmytran1.us.deloitte.com>
2021-07-26 11:59:43 -0400

    add thunk logic for removing a todo that pis hooked to server

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,4 +1,8 @@
-import { createTodo,loadTodosInProgress, loadTodosSuccess, loadTodosFailure } from './actions';
+import { createTodo,
+    removeTodo,
+    loadTodosInProgress,
+    loadTodosSuccess,
+    loadTodosFailure } from './actions';
 
 export const loadTodos = () => async (dispatch, getState) => {
     try {
@@ -25,6 +29,19 @@ export const addTodoRequest = text => async dispatch => {
         })
         const todo = await response.json()
         dispatch(createTodo(todo))
+    } catch (error) {
+        dispatch(displayAlert(error))
+    }
+}
+
+export const removeTodoRequest = id => async dispatch => {
+    try {
+        const response = await fetch(`http://localhost:8080/todos/${id}`, {
+            method: 'delete',
+        })
+        const removedTodo = await response.json()
+        dispatch(removeTodo(removedTodo))
+        
     } catch (error) {
         dispatch(displayAlert(error))
     }
